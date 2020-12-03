5896407668d58349b4d6d25f83f6ceec4674257b
Саша Плотников <plotnikovfw.adt@gmail.com>
2020-12-03 10:12:28 +0200

    Created simple configuration for our modal with next 4 methods:
    1. 'open' (with a lifehook 'onOpen');
    
    2. 'close' (with lifehooks 'onClose' and 'beforeClose');
    
    3. 'setContent' (replaces all the content except header with the html passed as an argument);
    
    4. 'destroy' (removes all the eventListeners and the modal itself from the DOM);

--- js/src/index.js
+++ js/src/index.js
@@ -1,3 +1,26 @@
 import { $ } from './base';
 
-$.modal();
+const modalContent = `
+  <main class="modal__main">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla consectetur deleniti adipisci et beatae fugit officia? Tenetur corrupti aperiam ut nostrum voluptatem, eos excepturi, sapiente illo obcaecati molestias voluptatum ex.
+  </main>
+  <footer class="modal__footer">
+    <button class="modal__confirm button">
+      Confirm
+    </button>
+    <button class="modal__cancel button">
+      Cancel
+    </button>
+  </footer>
+`;
+
+const options = {
+  title: "My modal",
+  closable: true,
+  content: modalContent,
+  width: '400px',
+}
+
+const myModal = $.modal(options);
+
+window.modal = myModal;
--- js/src/plugins/modal.js
+++ js/src/plugins/modal.js
@@ -1,5 +1,117 @@
-export function modal() {
-  console.log('works');
+function _createModal(options) {
+  const modal = document.createElement('div');
+  const defaultContent = `
+    <main class="modal__main">
+      modal_content
+    </main>
+    <footer class="modal__footer">
+      modal_footer
+    </footer>`;
+  
+  const {
+    title,
+    closable = true,
+    content = defaultContent,
+    width = '400px'
+  } = options;
+
+  modal.classList.add('modal');
+  modal.insertAdjacentHTML('afterbegin', `
+    <div class="modal__overlay">
+      <div class="modal__window" style="width: ${width}">
+        <div class="modal__content">
+          <header class="modal__header">
+            <h2 class="modal__title">${title}</h2>
+            ${closable
+              ? '<div class="modal__close">&times;</div>'
+              : ''
+            }
+          </header>
+          ${content}
+        </div>
+      </div>
+    </div>
+  `);
+
+  document.body.appendChild(modal);
+  return modal;
+}
+
+function closeModal(modal, isClosing, ANIMATION_TIME) {
+  isClosing = true;
+
+  modal.classList.remove('open');
+  modal.classList.add('closing');
+
+  setTimeout(() => {
+    modal.classList.remove('closing');
+    isClosing = false;
+  }, ANIMATION_TIME);
+}
+
+function closeHandler(e, modal, isClosing, ANIMATION_TIME) {
+  e.stopPropagation();
+
+  if (!e.target) {
+    console.error('Cannot detect event target after clicking on modal!')
+    return;
+  }
+
+  const isOverlay = e.target.classList.contains('modal__overlay'); 
+  const isCloseBtn = e.target.classList.contains('modal__close');
+  
+  if (isOverlay || isCloseBtn) {
+    closeModal(modal, isClosing, ANIMATION_TIME);
+  }
+}
+
+function _onOpen() {
+  console.log('Modal is opened!');
+}
+
+function _onClose() {
+  console.log('Modal is closed!');
+}
+
+function _beforeClose(isClosable) {
+  return isClosable ? true : false;
+}
+
+export function modal(options) {
+  const modal = _createModal(options);
+  const ANIMATION_TIME = 200; //ms
+  let isClosing = false;
+
+  const handleClosing = (e) => {
+    closeHandler(e, modal, isClosing, ANIMATION_TIME);
+  }
+  modal.addEventListener('click', handleClosing);
+
+  return {
+    open() {
+      !isClosing && modal.classList.add('open');
+      _onOpen();
+    },
+    close() {
+      if (!_beforeClose(true)) {
+        console.error('Modal cannot be closed!');
+        return;
+      }
+      closeModal(modal, isClosing, ANIMATION_TIME);
+      _onClose();
+    },
+    setContent(html) {
+      const content = modal.querySelector('.modal__content');
+      const header = modal.querySelector('.modal__header');
+
+      content.innerHTML = html;
+      content.insertAdjacentElement('afterbegin', header);
+    },
+    destroy() {
+      modal.removeEventListener('click', closeHandler);
+      modal.remove();
+    },
+  }
 }
 
 export default modal;
